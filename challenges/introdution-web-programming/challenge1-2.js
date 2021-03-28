21e98eea1068141c74d9b86f2e6cbcbd0327401d
Anderson Sanches <larsom6@gmail.com>
2021-03-28 10:08:31 -0400

    Challenge 1.2

---
+++ challenges/introdution-web-programming/challenge1-2.js
@@ -0,0 +1,34 @@
+//Contruction and printing of objects
+    const company = {
+        name: 'Rocketseat',
+        color: 'purple',
+        focus: 'Programming',
+        Address: {
+            street: 'Guilherme Gembala',
+            number: 260
+        }
+    }
+
+    console.log(`The company ${company.name} is location in address ${company.Address.street}, ${company.Address.number}.`)
+
+//Vectors and objects
+    const developer = {
+        name: 'Anderson',
+        age: 35,
+        skils: [
+            { 
+                name: 'C++', 
+                specialty: 'Desktop'
+            },
+            {
+                name: 'Python',
+                specialty: 'Data Science'
+            },
+            {
+                name: 'JavaScript',
+                specialty: 'Web/Mobile'
+            }
+        ]
+    }
+
+    console.log(`The developer ${developer.name} is years old and has ${developer.skils[0].name} skills with a specialty in ${developer.skils[0].specialty}.`)
